a7339020876614051de876cf86a8c28796ecfc34
piotrwita <piotr.witaszewski@gmai.com>
2021-12-17 23:22:30 +0100

    Moduł 2 - Praca domowa Analiza formatu JSON

---
+++ JsonExample/Post.cs
@@ -0,0 +1,18 @@
+﻿namespace JsonExample
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+
+        public Post()
+        {
+        }
+    }
+}
---
+++ JsonExample/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JsonExample;
+using System.Text.Json;
+
+//serializacja
+Post post = new Post("Tytuł", "Treść");
+string jsonString = JsonSerializer.Serialize(post);
+
+//deserializacja
+Post deserializePost = JsonSerializer.Deserialize<Post>(jsonString);
